8cce44ec5a327f0db242bfd1b28c7ba44255ac6b
Dhonrian <leonardo.tamanhao@gmail.com>
2019-05-14 09:39:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-b6anx6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-b6anx6)
---
+++ src/app/ambiente.ts
@@ -0,0 +1,10 @@
+export const ambiente = {
+  firebase: {
+    apiKey: "AIzaSyAi8g7HE97NHVXgQGw4Q98l5o7PhsUwOks",
+    authDomain: "usuarios-44865.firebaseapp.com",
+    databaseURL: "https://usuarios-44865.firebaseio.com",
+    projectId: "usuarios-44865",
+    storageBucket: "usuarios-44865.appspot.com",
+    messagingSenderId: "384559021396"
+  }
+};
